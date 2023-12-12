eb2b4330b61d17ec38536d2d1dc08c60624b366d
shahinmorve <133105811+shahinmorve@users.noreply.github.com>
2023-12-12 17:45:36 +0530

    added onchng function

--- src/component/LoginPage.jsx
+++ src/component/LoginPage.jsx
@@ -1,40 +1,42 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
-import password from "../assets/password.png";
-import profile from "../assets/profile.png";
-
 const LoginPage = () => {
+  const [action] = useState("signup");
+  const handleOnChange = () => {
+    console.log("working");
+  };
   return (
     <div className="container">
       <div className="header">
-        <div className="text">Signup </div>
+        <div className="text">{action} </div>
         <div className="underline"></div>
       </div>
       <div className="inputs">
         <div className="input">
-          <img src={profile} alt="Profile" />
-          <input type="text" />
+          <input type="text" placeholder="Name" />
+        </div>
+      </div>
+      <div className="inputs">
+        <div className="input" onChange={handleOnChange}>
+          <input type="Email" placeholder="email" />
         </div>
       </div>
       <div className="inputs">
         <div className="input">
-          <input type="Email" />
-        </div>
-      </div>
-      <div className="inputs">
-        <div className="input">
-          <img src={password} alt="Password" />
-          <input type="Password" />
+          <input type="Password" placeholder="password" />
         </div>
       </div>
       <div className="forgetPassword">forgetPassword</div>
       <div className="submit-container">
-        <div className="submit">signup</div>
-        <div className="submit">Login</div>
+        <div className={action === "signup" ? "submit gray" : "submit"}>
+          signup
+        </div>
+        <div className={action === "login" ? "submit gray" : "submit"}>
+          Login
+        </div>
       </div>
-
     </div>
   );
 };
